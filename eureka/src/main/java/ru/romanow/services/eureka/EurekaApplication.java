54773a848104bee7bf5d62edf55601515f5fdf76
a.romanov <a.romanov@softpro.com>
2018-12-10 19:38:07 +0300

    try start eureka

--- eureka/src/main/java/ru/romanow/services/eureka/EurekaApplication.java
+++ eureka/src/main/java/ru/romanow/services/eureka/EurekaApplication.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * nano /etc/hosts
- * 127.0.0.1	peer1
- * 127.0.0.1	peer2
- */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
