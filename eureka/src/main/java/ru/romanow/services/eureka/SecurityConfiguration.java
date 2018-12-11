a64908f7ca257bec1387e4a83beb8184768f431f
a.romanov <a.romanov@softpro.com>
2018-12-11 12:50:40 +0300

    eureka with security

---
+++ eureka/src/main/java/ru/romanow/services/eureka/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package ru.romanow.services.eureka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration
+        extends WebSecurityConfigurerAdapter {
+    private static final String USER_NAME = "eureka";
+    private static final String PASSWORD = "{noop}test";
+    private static final String ROLE = "admin";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin()
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(USER_NAME)
+                .password(PASSWORD)
+                .roles(ROLE);
+    }
+}
